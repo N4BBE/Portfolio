942a1dc8ac303f2af14e98a46429d75316d91cbd
Mohamed Nabeel <156911365+N4BBE@users.noreply.github.com>
2025-11-11 13:04:26 +0500

    New Changes

---
+++ Portfolio_1/assets/scripts/script.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const words = document.querySelectorAll('.nav-link'); // Select all navigation links
+    words.forEach(word => {
+        const originalText = word.textContent; // Store original text
+        const originalColor = window.getComputedStyle(word).color; // Store original color
+        word.addEventListener('mouseenter', () => {
+            word.textContent = '"' + originalText + '"'; // Add quotation marks
+            word.style.color = '#f2556fff'; // Change to highlight color
+        });
+        word.addEventListener('mouseleave', () => {
+            word.textContent = originalText; // Revert to original text
+            word.style.color = originalColor; // Revert to original color
+        });
+    });
+});
+
+// Menu Toggle Functionality
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+});
